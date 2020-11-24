0651fd4e6ebd9b8e2bcb0feec29fbfd996b23967
Verescako <74730039+Verescako@users.noreply.github.com>
2020-11-25 01:09:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-    let money = 8700,
-        income = "фриланс" , 
-        addExpenses = "Еда, Вода, Развлечения", 
-        deposit = true, 
-        mission = 156000,
-        period = 11,
-        budgetDay;
-    
-    
-    console.log(typeof money,typeof income,typeof deposit);
-    console.log(addExpenses.length);
-    console.log("Период равен " + period +" месяцев");
-    console.log("Цель заработать " + mission +" рублей");
+   "use strict";
+const num = 266219;
+let   str, arr = [], result;    // str - строка для преобразования числа в строку 
+                        
 
-    addExpenses = addExpenses.toLowerCase();
+    str = String(num);
+    arr = str.split('');
 
-    console.log(addExpenses.split(', '));
 
-    budgetDay = money / 30 ;
+    result = arr.reduce(function(sum, current) {
+        return  sum * current;
+    },);
+
  
-    console.log(budgetDay);
+    result **= 3;
+    str = String(result);
+    console.log(str.substring(0, 2));
